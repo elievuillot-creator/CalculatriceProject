7f5fcc90806c9a56f1c677bd5822ae4d4c9ee116
Vuillot <elie.vuillot@gmail.com>
2025-10-16 11:25:27 +0200

    Hey

---
+++ main.py
@@ -0,0 +1,17 @@
+class fictiveCALC():
+    def __init__:
+        None
+    def addition(self,a,b):
+        None
+    def soustraction(self,a,b):
+        None
+    def multiplication(self,a,b):
+        None
+    def division(self,a,b):
+        None
+    def fibonacci(self,a):
+        None
+    def exp(self, a):
+        None
+    def premier(self,a):
+        None
