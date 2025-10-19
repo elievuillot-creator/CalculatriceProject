1201a8dcbbc1f40a80b527309047b06bdd5d53e2
Vuillot <elie.vuillot@gmail.com>
2025-10-19 11:13:54 +0200

    version finish of functions

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-class fictiveCALC():
-    def __init__:
+class CALC():     # il va suffir ensuite de lacher des avec préalablement CALC = CALC() CALC.addition(a,b) pour avoir les résultats
+    def __init__(self):
         None
     def addition(self,a,b):
-        sum2 = a - b
-        return sum2
+        return a+b
     def soustraction(self,a,b):
-        None
+       return a-b
     def multiplication(self,a,b):
-        None
+        n=1
+        for i in range(int(b)):
+            n += a
     def division(self,a,b):
-        None
+        n = int(a)
+        PartInt = 0
+        while n > 0:
+            PartInt = PartInt +1
+            n-=int(b)
+        reste = int(a) - self.multiplication(b,PartInt)
+        return PartInt, reste
+
     def fibonacci(self,a):
-        None
+        fn2 = 1
+        fn1 = 0
+        for i in range(a):
+            result= fn1 + fn2
+            fn2=fn1
+            fn1=result
+        return result
+
+    def puissance(self,a, n):
+        result = 1
+        for i in range(int(n)):
+            result = self.multiplication(result,a)
+
     def exp(self, a):
-        None
+        self.puissance(2.7, a)
+
     def premier(self,a):
-        None
+        for i in range(2, int(a)):
+            x, y = self.division(a,i)
+            if y = 0:
+                print("Pas premier")
+                break
+        print("premier")
+
+CALC = CALC()
+print(CALC.puissance(2,8))
