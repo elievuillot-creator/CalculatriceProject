ea725b0d25b613b2da77fd1fc73ffbb17558abb7
zazou2004 <isabellamiralles12@gmail.com>
2025-10-24 09:27:54 +0200

    interface update, je recupère ce que l'utilisateur ecrtit au clavier. reste a pouvoir calculer en cliquant sur les boutons

--- Interfacegraphique.py
+++ Interfacegraphique.py
@@ -6,6 +6,10 @@ from tkinter import ttk
 window=tk.Tk()
 window.title("CALCULATRICE de Elie et Isa")
 #window.configure(bg="#483C32")
+def executercalcul():
+    entree=TXT_ent.get("1.0","end")
+    return entree
+
 
 #Frames
 frame_affichage=tk.Frame(window)
@@ -17,14 +21,14 @@ frm_bouton_chf.pack(expand=True,fill="both",side="left")
 frm_bouton_calcul=tk.Frame(window)
 frm_bouton_calcul.pack(expand=True,fill="both",side="right")
 
-entree=tk.Text(bg="#483C32",
+TXT_ent=tk.Text(bg="#483C32",
                 fg="white",
                 height=10,
                 width=50,
                 master=frame_affichage)
 
 
-entree.pack()
+TXT_ent.pack()
 
 chiffres=[(1,0,0),(2,0,1),(3,0,2),
          (4,1,0),(5,1,1),(6,1,2),
@@ -45,14 +49,13 @@ for c in range(3):
 
 calcul=[("+",0,0),("-",0,1),("*",0,2),
          ("/",1,0),('fibo',1,1),("premier",1,2),
-         ("e",2,0),("=",2,1),("",2,2)]
+         ("e",2,0),("!",2,1)]
 for chf,l,c in calcul:
-    if chf=="=":
-        bouton = tk.Button(master=frm_bouton_calcul, text=chf)
-        bouton.grid(row=l, column=c, ipadx=10, ipady=5, sticky="NSEW",columnspan=3)
-    else:
-        bouton=tk.Button(master=frm_bouton_calcul, text=chf)
-        bouton.grid(row=l,column=c,ipadx=10,ipady=5,sticky="NSEW")
+    bouton=tk.Button(master=frm_bouton_calcul, text=chf)
+    bouton.grid(row=l,column=c,ipadx=10,ipady=10,sticky="NSEW")
+
+BUT_exe=tk.Button(master=frm_bouton_calcul, text="calculer\n=",command=executercalcul)
+BUT_exe.grid(row=2,column=2,ipadx=10,ipady=10,sticky="NSEW")
 
 
 window.mainloop()
